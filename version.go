da595be07678ddefd63550b78f6292acbe469a61
unknown <pichit@olinhall.com>
2015-05-14 15:48:35 +0700

    old versoin

---
+++ version.go
@@ -0,0 +1,10 @@
+package gospf
+
+const (
+	// Current gospf version
+	VERSION = "0.12.0"
+	// Latest commit date
+	BUILD_DATE = "2015-03-25"
+	// Minimum required Go version
+	MINIMUM_GO = ">= go1.3"
+)
